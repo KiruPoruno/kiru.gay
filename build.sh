fe5f95ab8fed9b75a8611f924f5805e939d5788f
Kiru <mail@kiru.gay>
2021-12-12 20:26:21 +0100

    new: numbered/sorted blog posts
    Blog posts are now sorted by adding a number in front, so your first
    post will be "1 <post name>" and so on, the 1 and the whitespace will
    automatically be removed everywhere, so no need to worry.

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ rm -rf "$DEST"/*
 mkdir "$DEST"/blog "$DEST"/tmp -p
 
 write() {
-	FILE="$DEST/$2/$(echo "$i" | sed -e 's/\..*//g' -e 's/ /-/g' | tr "[:upper:]" "[:lower:]" | xargs basename).html"
+	FILE="$DEST/$2/$(echo "${i%.*}" | sed 's/ /-/g' | tr "[:upper:]" "[:lower:]" | xargs basename | sed 's/^\w*-//g').html"
 cat <<EOF > "$FILE"
 <html>
 	<head>
@@ -31,10 +31,10 @@ EOF
 }
 
 for i in "$SITE"/*.md; do write "$i";done
-for i in "$SITE"/posts/*.md; do	write "$i" "blog";done
-ls -tu "$SITE"/posts | grep -E '*.md' | while read i; do
-	LINK="$(echo $i | tr "[:upper:]" "[:lower:]" | sed -e 's/ /-/g' -e 's/.md/.html/g')"
-	echo "<br><div><a href="blog/$LINK">${i%.*}</a><br>Last modified: $(date -r "$SITE/posts/$i" +%Y-%m-%d\ -\ %I:%M:%S)</div>" >> "$DEST"/tmp/entries.html
+for i in "$SITE"/posts/*.md; do	write "$(echo "$i" | sed 's/^\w* //g')" "blog";done
+ls "$SITE"/posts | grep -E '*.md' | sort -r | while read i; do
+	LINK="$(echo $i | tr "[:upper:]" "[:lower:]" | sed -e 's/^\w* //g' -e 's/ /-/g' -e 's/.md/.html/g')"
+	echo "<br><div><a href="blog/$LINK">$(echo ${i%.*} | sed 's/^\w* //g')</a><br>Last modified: $(date -r "$SITE/posts/$i" +%Y-%m-%d\ -\ %I:%M:%S)</div>" >> "$DEST"/tmp/entries.html
 done
 
 for i in $(ls $PWD/"$DEST"/*.html $PWD/"$DEST"/blog/*.html); do
